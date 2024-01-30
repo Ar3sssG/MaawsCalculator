fd5fffaeb9479fd8e3ce69d6b6ec386d3a6e3588
Ar3sssG <116283922+Ar3sssG@users.noreply.github.com>
2024-01-30 18:24:51 +0400

    fixed elevation

--- scripts.js
+++ scripts.js
@@ -1,57 +1,66 @@
 'use strict'
-        function CalculateDistance(playerX, playerY, targetX, targetY) {
-            let xDiff = playerX - targetX;
-            let yDiff = playerY - targetY;
+function CalculateDistance(playerX, playerY, targetX, targetY) {
+    let xDiff = playerX - targetX;
+    let yDiff = playerY - targetY;
 
-            let distance = Math.sqrt(xDiff ** 2 + yDiff ** 2);
-            distance = Math.round(distance);
-            return distance;
-        }
+    let distance = Math.sqrt(xDiff ** 2 + yDiff ** 2);
+    distance = Math.round(distance);
+    return distance;
+}
 
-        function CalculateHorizontalRange(distance, azimuth = 0) {
-            return distance * Math.cos(azimuth);
-        }
+function CalculateHorizontalRange(distance, azimuth = 0) {
+    return distance * Math.cos(azimuth);
+}
 
-        function CalculateFlightTime(horizontalRange, projectileSpeed = 750) {
-            return horizontalRange / projectileSpeed;
-        }
+function CalculateFlightTime(horizontalRange, projectileSpeed = 750) {
+    return horizontalRange / projectileSpeed;
+}
 
-        function CalculateVerticalRange(flightTime, playerHeight, targetHeight, gravityForce = 9.81) {
-            let verticalRange = (0.5 * gravityForce * (flightTime ** 2)) + (targetHeight - playerHeight);
-            return verticalRange;
-        }
+function CalculateVerticalRange(flightTime, playerHeight, targetHeight, gravityForce = 9.81) {
+    let verticalRange = (0.5 * gravityForce * (flightTime ** 2)) + (targetHeight - playerHeight);
+    return verticalRange;
+}
 
-        function CalculateElevation(verticalRange, horizontalRange) {
-            return Math.atan(verticalRange / horizontalRange);
-        }
+function CalculateElevation(verticalRange, horizontalRange) {
+    return Math.atan(verticalRange / horizontalRange);
+}
 
-        function ConvertDegreesToMils(degree) {
-            return degree * 6400 / 360;
-        }
+function ConvertDegreesToMils(degree) {
+    return degree * 6400 / 360;
+}
 
-        function CalculateDistanceValue(){
-            let playerX = parseFloat(document.getElementById("playerX").value);
-            let playerY = parseFloat(document.getElementById("playerY").value);
-            let targetX = parseFloat(document.getElementById("targetX").value);
-            let targetY = parseFloat(document.getElementById("targetY").value);
+function CalculateDistanceValue() {
+    let playerX = parseFloat(document.getElementById("playerX").value);
+    let playerY = parseFloat(document.getElementById("playerY").value);
+    let targetX = parseFloat(document.getElementById("targetX").value);
+    let targetY = parseFloat(document.getElementById("targetY").value);
 
-            if(!isNaN(playerX) && !isNaN(playerY) && !isNaN(targetX) && !isNaN(targetY)) {
-                let distance = CalculateDistance(playerX, playerY, targetX, targetY);
-                document.getElementById("distanceResult").value = distance;
-                document.getElementById("targetDistance").value = distance;
-            }
+    if (!isNaN(playerX) && !isNaN(playerY) && !isNaN(targetX) && !isNaN(targetY)) {
+        let distance = CalculateDistance(playerX, playerY, targetX, targetY);
+        document.getElementById("distanceResult").value = distance;
+        document.getElementById("targetDistance").value = distance;
+    }
 
-            return;
-        }
+    return;
+}
 
-        function CalculateElevationAndETA() {
-            let targetDistance = parseFloat(document.getElementById("targetDistance").value);
-            let azimuth = parseFloat(document.getElementById("azimuth").value);
+function CalculateElevationAndETA() {
+    let targetDistance = parseFloat(document.getElementById("targetDistance").value);
+    let azimuth = parseFloat(document.getElementById("azimuth").value);
+    let playerHeight = parseFloat(document.getElementById("playerHeight").value);
+    let targetHeight = parseFloat(document.getElementById("targetHeight").value);
 
-            let horizontalRange = CalculateHorizontalRange(targetDistance, azimuth);
-            let flightTime = CalculateFlightTime(horizontalRange);
-            let verticalRange = CalculateVerticalRange(flightTime, playerHeight, targetHeight);
-            let elevation = CalculateElevation(verticalRange, horizontalRange);
-            let elevationInMils = ConvertDegreesToMils(elevation);
-            document.getElementById("elevationResult").innerText = elevationInMils;
-        }
+    if (!isNaN(targetDistance) || !isNaN(azimuth) || !isNaN(playerHeight) || !isNaN(targetHeight)) {
+        let horizontalRange = CalculateHorizontalRange(targetDistance, azimuth);
+        let flightTime = CalculateFlightTime(horizontalRange);
+        let verticalRange = CalculateVerticalRange(flightTime, playerHeight, targetHeight);
+        let elevation = CalculateElevation(verticalRange, horizontalRange);
+        let elevationInMils = ConvertDegreesToMils(elevation);
+
+        document.getElementById("elevationResult").value = Math.abs(elevationInMils);
+        document.getElementById("etaResult").value = flightTime;
+    }
+
+    return;
+
+}
